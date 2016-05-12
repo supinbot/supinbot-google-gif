05a28a23f0d0171696e64d16a82d84a3cf86eeb5
_FR_Starfox64 <louisdijon21@yahoo.fr>
2016-05-12 22:00:57 +0200

    Added googleimg command

--- index.js
+++ index.js
@@ -42,4 +42,43 @@ module.exports = function(SupinBot) {
 	})
 	.setDescription('Posts the first gif found on Google Image in the channel.')
 	.addArgument('Search Query', 'string');
+
+	SupinBot.CommandManager.addCommand('googleimg', function(user, channel, args, argsStr) {
+		google.customsearch('v1').cse.list({
+			q: argsStr,
+			searchType: 'image',
+			num: 1,
+			fields: 'items(link,title)',
+			cx: config.get('cx'),
+			key: config.get('api_key')
+		}, function(err, res) {
+			if (err) {
+				SupinBot.log.error('Google Search API: ' + err);
+				SupinBot.postMessage(channel.id, 'Google Search API: ' + err);
+				return;
+			}
+
+			if (res.items && res.items.length) {
+				SupinBot.postMessage(channel.id, null, {
+					attachments: [
+						{
+							title: res.items[0].title,
+							image_url: res.items[0].link
+						}
+					]
+				});
+			} else {
+				SupinBot.postMessage(channel.id, null, {
+					attachments: [
+						{
+							title: 'Image not found :(',
+							color: '#EA4335'
+						}
+					]
+				});
+			}
+		});
+	})
+	.setDescription('Posts the first image found on Google Image in the channel.')
+	.addArgument('Search Query', 'string');
 };
