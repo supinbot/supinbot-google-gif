c63d9d64fbc6fd714c08de948597cad19ae6620a
Starfox64 <louisdijon21@yahoo.fr>
2016-07-08 08:46:47 -0400

    Updated config for docker update

--- config.js
+++ config.js
@@ -1,14 +1,16 @@
 module.exports = function(config) {
-	return config.loadConfig('google-gif.json', {
+	return config.loadConfig({
 		api_key: {
 			doc: 'Your Google API Key',
 			format: String,
-			default: null
+			default: null,
+			env: 'SUPINBOT_GOOGLE_SEARCH_API'
 		},
 		cx: {
 			doc: 'Your custom Search Engine ID',
 			format: String,
-			default: null
+			default: null,
+			env: 'SUPINBOT_GOOGLE_SEARCH_ID'
 		}
 	});
 };
